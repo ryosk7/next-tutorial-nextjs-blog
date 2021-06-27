36f37f051ce19bb33dd9f5b72111718a80b11657
ryosk7 <uchiryo7@gmail.com>
2021-06-27 19:41:25 +0900

    feat(next): tutorial

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (<>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </Layout>
+  </>
+  )
+}
